680a9fbf62eab7fbea61b0d3e12e0cdf17cf9fa6
Tadeusz Kozak <stoiczek@gmail.com>
2012-07-13 15:02:34 +0200

    Defined "externs" for WebRTC API to for easier API reference

---
+++ call_app/client/js/WebRTCExterns.js
@@ -0,0 +1,136 @@
+//noinspection JSUnusedLocalSymbols
+
+
+/**
+ *
+ *
+ * @param {String} iceConfig
+ * @param {Function} iceListener
+ * @constructor
+ */
+function PeerConnection(iceConfig, iceListener) {
+  //noinspection JSUnusedGlobalSymbols
+  this.onconnecting = function () {
+  };
+  this.onopen = function () {
+  };
+  this.onaddstream = function () {
+  };
+  this.onremovestream = function () {
+  };
+
+  /**
+   *
+   * @type {String}
+   */
+  this.SDP_OFFER = "";
+  /**
+   *
+   * @type {String}
+   */
+  this.SDP_ANSWER = "";
+}
+
+/**
+ *
+ */
+PeerConnection.prototype.close = function () {
+
+};
+
+/**
+ *
+ */
+PeerConnection.prototype.startIce = function () {
+
+};
+
+/**
+ *
+ * @param {MediaStream}str
+ */
+PeerConnection.prototype.addStream = function (str) {
+
+};
+
+
+/**
+ *
+ * @param {Object} contents
+ * @param {Boolean} contents.audio
+ * @param {Boolean} contents.video
+ */
+PeerConnection.prototype.createOffer = function (contents) {
+
+}
+
+;
+/**
+ *
+ * @param {SessionDescription} sdp
+ * @param {Object} contents
+ * @param {Boolean} contents.audio
+ * @param {Boolean} contents.video
+ */
+PeerConnection.prototype.createAnswer = function (sdp, contents) {
+
+};
+
+/**
+ *
+ * @param {String} type
+ * @param {SessionDescription} sdp
+ */
+PeerConnection.prototype.setLocalDescription = function (type, sdp) {
+
+};/**
+ *
+ * @param {String} type
+ * @param {SessionDescription} sdp
+ */
+PeerConnection.prototype.setRemoteDescription = function (type, sdp) {
+
+};
+
+/**
+ *
+ * @param {IceCandidate} iceCandidate
+ */
+PeerConnection.prototype.processIceMessage = function (iceCandidate) {
+
+};
+
+/**
+ *
+ * @param {String} sdpAsStr
+ * @constructor
+ */
+function SessionDescription(sdpAsStr) {
+
+}
+
+/**
+ *
+ * @constructor
+ */
+function MediaStream() {
+}
+
+/**
+ *
+ * @constructor
+ */
+function IceCandidate() {
+  /**
+   *
+   * @type {String}
+   */
+  this.label = '';
+}
+
+/**
+ * @return {String}
+ */
+IceCandidate.prototype.toSdp = function () {
+  return '';
+};
